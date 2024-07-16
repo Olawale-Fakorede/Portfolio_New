7f92717f9122ae8b5282e662ec3470e27e6f1830
unknown <olawaleprofjobs15@gmail.com>
2024-07-16 15:40:57 -0700

    tools component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Header from './components/Header';
+import About from './components/About';
+import Tools from './components/Tools';
 
 function App() {
   return (
-    <div className="App">
+    <div className='w-full h-full bg-gradient-to-r from-white to-fuchsia-500'>
       
         <Header />
+        <About />
+        <Tools />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full h-screen bg-white rounded-tr-3xl mb'>
+        <h1 className='text-blue-200 font-extrabold text-7xl pl-96 ml-40 pt-7'>
+          About me
+        </h1>
+        
+        <hr/>
+    </div>
+  )
+}
+
+export default About
