3a8f38a9072aad3ace2fd0ef1d51560840cac3ff
unknown <olawaleprofjobs15@gmail.com>
2024-07-21 04:13:17 -0700

    Header and Hero section done

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,25 @@
 
 import React from 'react'
+import myImage from './images/imageWale.jpg'
 
 const About = () => {
   return (
-    <div className='w-full h-screen bg-white rounded-tr-3xl mb'>
-        <h1 className='text-blue-200 font-extrabold text-7xl pl-96 ml-40 pt-7'>
+    <div className='w-full h-fit bg-white rounded-tr-3xl grid text-center justify-center'>
+        <h1 className='text-blue-200 font-extrabold text-5xl md:text-6xl lg:text-7xl xl:text-9xl pl-20 md:pl-16 lg:pl-52 pt-7'>
           About me
         </h1>
-        
+        <br/>
+        <div className='w-full h-fit grid grid-cols-1 gap-2 mb-9 relative left-2 lg:left-9 justify-center'>
+          <img className='w-64 h-64 md:w-72 md:h-72 lg:w-96 lg:h-96 relative left-10 lg:mr-24' src={myImage} alt="" />
+          <div className='w-42 h-fit overflow-scroll text-sm md:text-xl lg:text-5xl md:w-72 md:h-72 lg:w-full lg:h-full pl-4 pt-4 pr-3 text-start font-sans mt-3'>
+            I'm a Full Stack Developer who loves gaining experiences in new projects.
+            I have experoence working with HTML, CSS, JavaScript, ReactJS, NextJS, Tailwind css,
+            NodeJS, ExpressJS, MongoDb, and Git. I'm passionate and concerned about delivering user-friendly
+            and responsive Web applications as well as solutions to problems.
+
+
+          </div>
+        </div>
         <hr/>
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,28 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full h-72 rounded-bl-3xl bg-gradient-to-r from-violet-500 to-fuchsia-500'>
+    <div className='w-full min-w-full max-w-full h-80 md:h-64 lg:h-66 rounded-bl-3xl bg-gradient-to-r from-violet-500 to-fuchsia-500'>
 
-        <div className='ml-16 pt-6 flex'>
-            <h1 className='font-extrabold mr-96 text-start font-sans text-slate-200'>
-                Olawale Fakorede
+        <div className='pl-4 md:pl-12 pt-6 w-full flex flex-row'>
+            <h1 className='font-bold text-start font-sans text-slate-200'>
+                PORTFOLIO
             </h1>
-            <p className='ml-24 text-slate-200 text-sm hover:text-amber-300 hover:font-semibold'>About</p>
-            <p className='ml-12 text-slate-200 text-sm hover:text-amber-300 hover:font-semibold'>Skills</p>
-            <p className='ml-12 text-slate-200 text-sm hover:text-amber-300 hover:font-semibold'>My Projects</p>
-            <p className='ml-12 text-slate-200 text-sm hover:text-amber-300 hover:font-semibold'>Contact</p>
+            <p className='pl-12 md:pl-44 lg:pl-96 text-slate-200 text-xs md:text-lg lg:text-2xl hover:text-amber-300 hover:font-semibold'>About</p>
+            <p className='pl-2 md:pl-6 lg:pl-9 text-slate-200 text-xs md:text-lg lg:text-2xl hover:text-amber-300 hover:font-semibold'>Skills</p>
+            <p className='pl-2 md:pl-6 lg:pl-9 text-slate-200 text-xs md:text-lg lg:text-2xl hover:text-amber-300 hover:font-semibold'>My Projects</p>
+            <p className='pl-2 md:pl-6 lg:pl-9 text-slate-200 text-xs md:text-lg lg:text-2xl hover:text-amber-300 hover:font-semibold'>Contact</p>
             
         </div>
 
-        <div className='pt-5 pl-16 font-bold font-sans text-xl pb-10'>
+        <div className='pt-5 pl-6 md:pl-12 font-medium font-sans text-sm pb-10'>
           <h2>Hi,</h2>
           <h2>I am Olawale,</h2>
-          <h2>A Frontend Developer,</h2>
-          <h2>& a Tutor.</h2>
+          <h2>A Full Stack Developer,</h2>
+          <h2>& Tutor.</h2>
           <h2>I create user freindly interface.</h2>
         </div>
 
-        <span className='h-7 pt-20 pl-24'>
+        <span className='h-7 pt-6 pl-6 md:pl-12'>
 
           Download my Resume
         </span>
